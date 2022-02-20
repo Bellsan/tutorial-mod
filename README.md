3d6261c2d78e1fa04c7d22b95db6a468316f063f
Elliot Steffensen <elliot@steffensens.se>
2022-02-20 09:38:41 +0100

    Setup workspace

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
-# Fabric Example Mod
+# Tutorial mod
 
-## Setup
+Just a simple mod to learn how to use fabric
 
-For setup instructions please see the [fabric wiki page](https://fabricmc.net/wiki/tutorial:setup) that relates to the IDE that you are using.
-
-## License
-
-This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
--- src/main/java/io/steffensen/tutorialmod/mixin/TutorialMixin.java
+++ src/main/java/io/steffensen/tutorialmod/mixin/TutorialMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package io.steffensen.tutorialmod.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import io.steffensen.tutorialmod.TutorialMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class TutorialMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		TutorialMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
